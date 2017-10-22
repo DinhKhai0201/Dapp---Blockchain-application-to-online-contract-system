68d78627aca13709f5b2d34b1a7d33dcf7110980
Mike Polinowski <mpolinowski@users.noreply.github.com>
2017-10-22 18:49:10 +0800

    nested routes update

--- src/components/NestedRoutes.js
+++ src/components/NestedRoutes.js
@@ -1,94 +1,89 @@
 import React from 'react'
-import {
-  Route,
-  NavLink
-} from 'react-router-dom'
-import { Button } from 'react-bootstrap'
+import { Route, NavLink, Link } from 'react-router-dom'
 
+import { PageOneNested } from './PageOneNested'
+import { PageTwoNested } from './PageTwoNested'
 
 const Topics = ({ match }) => (
 
     <div className="container-fluid">
-      <div className="clearfix"><br/><br/><br/></div>
       <div className="row">
-        <nav className="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
-          <ul className="nav nav-pills flex-column">
-            <li className="nav-item">
-              <NavLink class="nav-link" to={`${match.url}/page-1-nested`}>
-                <Button bsStyle="primary" block>NestOne</Button><br/>
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink class="nav-link" to={`${match.url}/page-2-nested`}>
-                <Button bsStyle="primary" block>NestTwo</Button>
-              </NavLink>
-            </li>
-          </ul>
+        <nav className="nav nav-pills nav-fill col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+          <div className="clearfix"><br/><br/><br/></div>
+          <Link className="nav-item nav-link" to={match.url}>
+            <h3>Nested Routes</h3>
+            <hr/>
+          </Link>
+          <NavLink className="nav-item nav-link" to={`${match.url}/PageOneNested/`}>
+            NestOne
+          </NavLink>
+          <NavLink className="nav-item nav-link" to={`${match.url}/PageTwoNested/`}>
+            NestTwo
+          </NavLink>
         </nav>
 
         <main role="main" className="col-sm-9 ml-sm-auto col-md-10 pt-3">
+          <div className="clearfix"><br/><br/><br/></div>
           <h1>Dashboard</h1>
-
-          <h2>Section title</h2>
-          <div className="table-responsive">
-            <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>Lorem</td>
-                  <td>ipsum</td>
-                  <td>dolor</td>
-                  <td>sit</td>
-                </tr>
-                <tr>
-                  <td>1,002</td>
-                  <td>amet</td>
-                  <td>consectetur</td>
-                  <td>adipiscing</td>
-                  <td>elit</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>Integer</td>
-                  <td>nec</td>
-                  <td>odio</td>
-                  <td>Praesent</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>libero</td>
-                  <td>Sed</td>
-                  <td>cursus</td>
-                  <td>ante</td>
-                </tr>
-                <tr>
-                  <td>1,004</td>
-                  <td>dapibus</td>
-                  <td>diam</td>
-                  <td>Sed</td>
-                  <td>nisi</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <Route exact path={`${match.url}/:topicId`} component={Topic}/>
+          <Route exact path={match.url} render={() => (
+            <div>
+              <h2>Lorem Table</h2>
+              <div className="table-responsive">
+                <table className="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Header</th>
+                      <th>Header</th>
+                      <th>Header</th>
+                      <th>Header</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1,001</td>
+                      <td>Lorem</td>
+                      <td>ipsum</td>
+                      <td>dolor</td>
+                      <td>sit</td>
+                    </tr>
+                    <tr>
+                      <td>1,002</td>
+                      <td>amet</td>
+                      <td>consectetur</td>
+                      <td>adipiscing</td>
+                      <td>elit</td>
+                    </tr>
+                    <tr>
+                      <td>1,003</td>
+                      <td>Integer</td>
+                      <td>nec</td>
+                      <td>odio</td>
+                      <td>Praesent</td>
+                    </tr>
+                    <tr>
+                      <td>1,003</td>
+                      <td>libero</td>
+                      <td>Sed</td>
+                      <td>cursus</td>
+                      <td>ante</td>
+                    </tr>
+                    <tr>
+                      <td>1,004</td>
+                      <td>dapibus</td>
+                      <td>diam</td>
+                      <td>Sed</td>
+                      <td>nisi</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          )}/>
         </main>
       </div>
-
-      <Route path={`${match.url}/:topicId`} component={Topic}/>
-      <Route exact path={match.url} render={() => (
-        <h3>Please select a topic.</h3>
-      )}/>
-
-  </div>
+    </div>
 )
 
 const Topic = ({ match }) => (
